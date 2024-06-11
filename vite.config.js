c953a82c08857644510b49b2a94294cd54810a24
Miguel <migue@DESKTOP-08KN104.(none)>
2024-06-11 20:26:11 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173, // You can change this port if needed
+    strictPort: true, // Make sure the port is used, otherwise exit
+  },
+});
